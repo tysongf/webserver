e5559625a7193b5f6ba77ea9c5e911723377c645
Tyson Fritz <tysongfritz@gmail.com>
2022-08-22 10:22:09 -0700

    Handle request endpoints 'planet' and 'exoplanet'. Default to 404 Not Found

--- webserver.js
+++ webserver.js
@@ -1,11 +1,48 @@
 const http = require('http');
-const { reset } = require('nodemon');
 
 const HTTP_PORT = 3000;
 
 const server = http.createServer((req, res) => {
+
    res.writeHead(200, { content: 'application/json' });
-   res.end(JSON.stringify({ message: 'Hello Web!', port: HTTP_PORT}));
+   let json_response = { 'message': 'Resource not found.' };
+
+   switch(req.url) {
+      case '/planets':
+         json_response = {
+            planets: [
+               { id: 1, name: 'Mercury' },
+               { id: 2, name: 'Venus' },
+               { id: 3, name: 'Earth' },
+               { id: 4, name: 'Mars' },
+               { id: 5, name: 'Jupiter' },
+               { id: 6, name: 'Saturn' },
+               { id: 7, name: 'Uranus' },
+               { id: 8, name: 'Neptune' }
+            ]
+         };
+      break;
+      case '/exoplanets':
+         json_response = {
+            planets: [
+               { id: 1, name: 'Kepler-22b' },
+               { id: 2, name: 'Kepler-442b' },
+               { id: 3, name: 'Kepler-1652b' },
+               { id: 4, name: 'Kepler-1410b' },
+               { id: 5, name: 'Kepler-296f' },
+               { id: 6, name: 'Kepler-296e' },
+               { id: 7, name: 'Kepler-1649b' },
+               { id: 8, name: 'Kepler-62f' }
+            ]
+         }
+      break;
+      default:
+         res.statusCode = 404;
+   }
+
+   res.end(JSON.stringify(json_response));
+
+   
 });
 
 server.listen(HTTP_PORT, () => {
