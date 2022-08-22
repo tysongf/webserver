5c5843648be4650f09c310a1507038b17c88ffa1
Tyson Fritz <tysongfritz@gmail.com>
2022-08-22 09:59:50 -0700

    JSON response and parameterized HTTP_PORT

--- webserver.js
+++ webserver.js
@@ -1,9 +1,13 @@
 const http = require('http');
 const { reset } = require('nodemon');
 
+const HTTP_PORT = 3000;
+
 const server = http.createServer((req, res) => {
-   res.writeHead(200, { content: 'text/plain' });
-   res.end('Hello Web!');
+   res.writeHead(200, { content: 'application/json' });
+   res.end(JSON.stringify({ message: 'Hello Web!', port: HTTP_PORT}));
 });
 
-server.listen(80);
+server.listen(HTTP_PORT, () => {
+   console.log(`Listening on port ${HTTP_PORT}`);
+});
